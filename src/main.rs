b7d6c7770f36514e8ac2affa453c466401b915a5
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-10-27 10:37:58 +0100

    Feat(example.rs): Example Route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+use actix_web::{HttpServer, App};
+
+use routes::example::hello_world;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello_world)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
